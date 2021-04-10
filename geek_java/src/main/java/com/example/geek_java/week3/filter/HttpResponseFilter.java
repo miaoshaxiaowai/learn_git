57fb79c2fa92b6ffa4f84e0246c10427bc6a4b62
GeekUniversity <1016588149@qq.com>
2021-04-10 22:53:03 +0800

    删除文件

--- geek_java/src/main/java/com/example/geek_java/week3/filter/HttpResponseFilter.java
+++
@@ -1,9 +0,0 @@
-package com.example.geek_java.week3.filter;
-
-import io.netty.handler.codec.http.FullHttpResponse;
-
-public interface HttpResponseFilter {
-
-    void filter(FullHttpResponse response);
-
-}
